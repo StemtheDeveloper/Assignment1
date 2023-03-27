1bfda6b785da48222562fa6311c05a32e704a3cd
StemtheDeveloper <127262716+StemtheDeveloper@users.noreply.github.com>
2023-03-27 13:57:44 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// setTimeout(function() {
+//     animationCon.value = '';
+//   }, 5000);
+  
+
+// function deleteAnimation() {
+//     document.getElementsByClassName('animationCon').value = '';
+// }
+
